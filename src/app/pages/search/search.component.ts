a534f1d94e6c7813f46f750f6c64115289be5a74
cintiatilly <joannisreal@gmail.com>
2020-03-07 15:26:25 -0300

    ADD cards

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -2,10 +2,58 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ngx-search',
-  template: `
-  <p>SEARSHCOWUR</p>
-  `,
+  templateUrl: `search.component.html`,
+  styleUrls: ['./search.scss'],
 })
 export class SearchComponent {
+
+
+
+
+
+  dataBooks = [
+    {
+      titulo: 'O principe e o mendigo',
+      autor:  'Mark Twain',
+      pais: 'França',
+      genero: 'Romance Histórico',
+    },
+    {
+      titulo: 'A Menina que Roubava Livros',
+      autor:  'Markus Zusak',
+      pais: 'Alemanha',
+      genero: 'Ficção Histórica',
+    },
+    {
+      titulo: 'A Marca de uma Lágrima',
+      autor:  'Pedro Bandeira',
+      pais: 'Brasil',
+      genero: 'Romance',
+    },
+    {
+      titulo: 'O Homem que Calculava',
+      autor:  'Malba Tahan',
+      pais: 'India',
+      genero: 'Romance',
+    },
+    {
+      titulo: 'A Marca de uma Lágrima',
+      autor:  'Pedro Bandeira',
+      pais: 'Brasil',
+      genero: 'Romance',
+    },
+    {
+      titulo: 'A Marca de uma Lágrima',
+      autor:  'Pedro Bandeira',
+      pais: 'Brasil',
+      genero: 'Romance',
+    },
+    {
+      titulo: 'A Marca de uma Lágrima',
+      autor:  'Pedro Bandeira',
+      pais: 'Brasil',
+      genero: 'Romance',
+    },
+  ];
 
 }
